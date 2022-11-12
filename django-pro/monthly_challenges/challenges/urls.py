d4e2111a53cb65bd260c1c4c4cf844e349997b4c
Trung <phantrung282@gmail.com>
2022-11-13 02:43:14 +0700

    Including Partial Template Snippets

--- django-pro/monthly_challenges/challenges/urls.py
+++ django-pro/monthly_challenges/challenges/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path("february", views.february),
     # path("march", views.march)
 
-    path("",views.index), #challenges
+    path("",views.index, name="index"), #challenges
     path("<int:month>",views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenges, name="month_challenge")
 ]
